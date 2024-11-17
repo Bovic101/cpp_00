38cd153acba5875cba2a62be1373e0e4e40a1e95
Bovic101 <Victorbolanle72@gmail.com>
2024-11-17 11:57:16 +0100

    megaphone implementation

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,14 +6,35 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 10:01:59 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/17 10:46:35 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/17 11:53:55 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-int	main(int argc, char **argv)
+int i;
+int j;
+
+int main(int argc, char** argv) 
 {
-	
+    if (argc < 2) 
+	{
+        std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE\n";
+        return 0;
+    }
+
+    j = 1;
+    while (j < argc)
+	{
+        i = 0;
+        while (argv[j][i] != '\0') 
+		{
+            std::cout << static_cast<char>(std::toupper(argv[j][i]));
+            i++;
+        }
+        j++;
+    }
+    std::cout << "\n";
+    return 0;
 }
