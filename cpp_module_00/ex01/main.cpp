e1b012f2fa020e9324fea0c5d22b3141909f6311
Bovic101 <Victorbolanle72@gmail.com>
2024-11-18 14:04:39 +0100

    partial implementation of the main programm:main.cpp

--- cpp_module_00/ex01/main.cpp
+++ cpp_module_00/ex01/main.cpp
@@ -6,7 +6,38 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:00:03 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/18 11:00:04 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:02:24 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "ClassPhonebook.hpp"
+
+int main()
+{
+	//PhoneBook phone;// To implement
+	std::string cmd;
+	
+	while (true)
+	{
+		std::cout<<"Enter a command to proceed(ADD, SEARCH, EXIT):";
+		std::getline(std::cin, cmd);
+
+		if (cmd =="ADD")
+		{
+			/* To do phone implementation */
+		}
+		else if (cmd == "SEARCH")
+		{
+			/* To do */
+		}
+		else if (cmd =="EXIT")
+		{
+			std::cout<<"Quiting phonebook....\n";
+			break;
+		}
+		else
+			std::cout<<"Invalid input command\n";
+	}
+	return(0);
+}
