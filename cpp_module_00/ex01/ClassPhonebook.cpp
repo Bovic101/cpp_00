48afad122543b333d8b494bb089279f70559f6b8
Bovic101 <Victorbolanle72@gmail.com>
2024-11-19 15:43:18 +0100

    addcontact function implementation in the classphobook.cpp

--- cpp_module_00/ex01/ClassPhonebook.cpp
+++ cpp_module_00/ex01/ClassPhonebook.cpp
@@ -6,8 +6,63 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:00:26 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/18 13:06:22 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:40:52 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClassPhonebook.hpp"
+#include <iostream>
+#include <iomanip>
+
+PhoneBook::PhoneBook() : oldest_contact(0), contact_counter(0) {}
+
+void PhoneBook::add_to_contact() {
+    Contact newEntry;
+    std::string input;
+
+    std::cout << "Enter Initial/First Name: ";
+    while (std::getline(std::cin, input) && input.empty()) 
+	{
+        std::cout << "First Name is compulsory. Retype: ";
+    }
+    newEntry.setInitialName(input);
+
+    std::cout << "Enter Last Name: ";
+    while (std::getline(std::cin, input) && input.empty()) 
+	{
+        std::cout << "Last Name is compulsory. Retype: ";
+    }
+    newEntry.setLastName(input);
+
+    std::cout << "Enter Nickname: ";
+    while (std::getline(std::cin, input) && input.empty()) 
+	{
+        std::cout << "Nickname is compulsory. Retype: ";
+    }
+    newEntry.setNickName(input);
+
+    std::cout << "Enter Mobile Number: ";
+    while (std::getline(std::cin, input) && input.empty()) 
+	{
+        std::cout << "Mobile Number is compulsory. Retype: ";
+    }
+    newEntry.setMobileNumber(input);
+
+    std::cout << "Enter Dark Secret: ";
+    while (std::getline(std::cin, input) && input.empty()) 
+	{
+        std::cout << "Dark Secret is compulsory. Retype: ";
+    }
+    newEntry.setDarkSecret(input);
+
+    if (contact_counter < 8) 
+	{
+        max_contact[contact_counter++] = newEntry;
+    } 
+	else 
+	{
+        max_contact[oldest_contact] = newEntry;
+        oldest_contact = (oldest_contact + 1) % 8;
+    }
+}
 
--- cpp_module_00/ex01/ClassPhonebook.hpp
+++ cpp_module_00/ex01/ClassPhonebook.hpp
@@ -6,7 +6,27 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:00:14 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/18 11:00:15 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/19 12:34:53 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "ClassContact.hpp"
+#include <iostream>
+#include <iomanip>
+
+class PhoneBook{
+	private:
+	Contact max_contact[8];
+	int oldest_contact;
+	int contact_counter;
+	
+	public:
+	PhoneBook();//to implement
+	void add_to_contact();
+	void search_for_contact() const;
+};
+
+#endif
